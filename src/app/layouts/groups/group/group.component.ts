44990947b0c24137b62413998b9348c0b619347a
Artem Serebryakov <artem.serebryakov@nih.gov>
2019-02-28 22:55:38 -0500

    refactor(layout): refactored group layout

--- src/app/layouts/groups/group/group.component.ts
+++ src/app/layouts/groups/group/group.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldConfig, ILookup, PanelGroup, ConditionType } from '../../../../types'; // todo: move specific types here
 import { BaseLayout } from '../../base-layout';
+import { Subscription } from 'rxjs';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'layout-group-group',
@@ -9,9 +11,15 @@ import { BaseLayout } from '../../base-layout';
 })
 export class GroupComponent extends BaseLayout implements OnInit {
 
+    public ref = { groups: [] };
+    private subscriptions: Subscription[] = [];
+    public shown = [];
+
     public ngOnInit(): void {
+
         let fields: FieldConfig[] = [];
-        this.formConfig.form.forEach(gr => {
+        this.formConfig.form.forEach((gr, index) => {
+            this.shown.push({ hidden: index > 0, valid: false, controls: [] });
             if (gr.fields) { fields = fields.concat(gr.fields); }
             if (gr.panels) {
                 gr.panels.forEach(panel => {
@@ -67,6 +75,64 @@ export class GroupComponent extends BaseLayout implements OnInit {
         // leave if() conditions in case other logic operators needed (XOR, etc)
 
         return enabled;
+    }
+
+
+    ngAfterViewInit() {
+
+        this.formConfig.form.forEach((group, index) => {
+            let fields = [];
+
+            if (group.fields) {
+                fields = fields.concat(group.fields);
+            }
+
+            if (group.panels) {
+                group.panels.forEach(panel => { if (panel.fields) { fields = fields.concat(panel.fields); } });
+            }
+
+            this.shown[index].controls = fields.filter(f => f.required).map(f => this.group.get(f.name));
+
+            if (this.shown[index].controls.length === 0) { this.shown[index].valid = true; } else {
+                this.shown[index].controls.forEach((control: AbstractControl) => {
+                    this.subscriptions.push(control.statusChanges.subscribe(() => {
+                        this.shown[index].valid = this.shown[index].controls.every((ctrl: AbstractControl) => ctrl.valid);
+                    }));
+                });
+            }
+        });
+    }
+
+    public selected = 0;
+
+    select(index: number): void {
+        this.shown[this.selected].hidden = true;
+        this.shown[index].hidden = false;
+        this.selected = index;
+    }
+
+    public getSelected() {
+        return this.selected;
+    }
+
+    public prev() {
+        this.select(this.selected - 1);
+    }
+
+    public next() {
+        this.select(this.selected + 1);
+    }
+
+    public disablePrev() {
+        return this.selected < 1;
+    }
+
+    public disableNext() {
+        return this.selected > this.shown.length - 2;
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach((sub: Subscription) => sub.unsubscribe());
     }
 
 }
--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -2,7 +2,6 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutDictionary } from '../../types';
 import { GroupComponent } from './groups/group/group.component';
-import { FormNavModule } from './groups/nav/nav-app';
 import { PanelComponent } from './groups/panel/panel.component';
 import { DynamicFieldModule } from '../dynamic-field/dynamic-field.module';
 import { BasicLayoutComponent } from './basic/basic-layout.component';
@@ -15,7 +14,6 @@ export const defaultLayouts: LayoutDictionary = {
 @NgModule({
     imports: [
         CommonModule,
-        FormNavModule,
         DynamicFieldModule
     ],
     declarations: [
@@ -33,6 +31,3 @@ export const defaultLayouts: LayoutDictionary = {
     ]
 })
 export class FormLayoutsModule { }
-
-// todo: merge nav and panel into one layout component if possible
-// todo: refactor this like fields
